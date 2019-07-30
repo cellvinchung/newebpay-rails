511d7cf8fc09101d23914eb15bc8cd3c365a9c95
鍾劭方（Cellvin Chung） <cellvinchung@gmail.com>
2019-07-30 15:14:25 +0800

    protect_from_forgery prepend: true

--- app/controllers/newebpay/application_controller.rb
+++ app/controllers/newebpay/application_controller.rb
@@ -1,7 +1,7 @@
 module Newebpay
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :null_session
-
+    protect_from_forgery prepend: true
     private
 
     def newebpay_response
