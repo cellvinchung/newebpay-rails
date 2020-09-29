13af71ee5648fec45b302a37cc92d9a3974a5cff
chagn <ycw99@hotmail.com>
2020-09-29 21:18:28 +0800

    改用match

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Newebpay::Engine.routes.draw do
-  post 'mpg_callbacks', to: 'mpg_callbacks#proceed'
-  post 'payment_code_callbacks', to: 'payment_code_callbacks#proceed'
-  post 'notify_callbacks', to: 'notify_callbacks#proceed'
+	match :build, action: :new, via: [:get, :post]
 
-  post 'periodical_callbacks', to: 'periodical_callbacks#proceed'
-  post 'periodical_notify_callbacks', to: 'periodical_notify_callbacks#proceed'
 
-  get 'donation_callbacks', to: 'donation_callbacks#proceed'
-  post 'donation_notify_callbacks', to: 'donation_notify_callbacks#proceed'
+	match 'mpg_callbacks', to: 'mpg_callbacks#proceed', via: [:get, :post]
+	match 'payment_code_callbacks', to: 'payment_code_callbacks#proceed', via: [:get, :post]
+	match 'notify_callbacks', to: 'notify_callbacks#proceed', via: [:get, :post]
 
-  post 'cancel_auth_notify_callbacks', to: 'cancel_auth_notify_callbacks#proceed'
+	match 'periodical_callbacks', to: 'periodical_callbacks#proceed', via: [:get, :post]
+	match 'periodical_notify_callbacks', to: 'periodical_notify_callbacks#proceed', via: [:get, :post]
+
+	match 'donation_callbacks', to: 'donation_callbacks#proceed', via: [:get, :post]
+	match 'donation_notify_callbacks', to: 'donation_notify_callbacks#proceed', via: [:get, :post]
+
+	match 'cancel_auth_notify_callbacks', to: 'cancel_auth_notify_callbacks#proceed', via: [:get, :post]
 end
