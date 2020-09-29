e94f4186cf971203476d5852ed993e65fad9da81
chagn <ycw99@hotmail.com>
2020-09-29 21:10:17 +0800

    處理 result

--- lib/newebpay/periodical/response.rb
+++ lib/newebpay/periodical/response.rb
@@ -2,14 +2,14 @@
 
 module Newebpay::Periodical
   class Response
-    attr_reader :status, :message
+    attr_reader :status, :message, :result
     def initialize(response_params)
       raw_params = Newebpay::NewebpayHelper.decrypt_data(response_params)
 
       response_data = JSON.parse(raw_params)
       @status = response_data['Status']
       @message = response_data['Message']
-      @result = response_data['Result']
+      @result = response_data['Result']&.deep_transform_keys{ |key| key.to_s.underscore }
 
       @result.each do |key, values|
         define_singleton_method(key.underscore) do
